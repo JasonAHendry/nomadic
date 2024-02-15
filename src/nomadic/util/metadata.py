e737b49b7e8fd84fdfc4d51e7e5757ea74ff1fff
JasonAHendry <jason.ahendry@gmail.com>
2024-02-15 13:08:44 +0100

    More robust parsing of user-supplied metadata CSV.

--- src/nomadic/util/metadata.py
+++ src/nomadic/util/metadata.py
@@ -1,5 +1,77 @@
+import re
+import warnings
 import pandas as pd
+from typing import List
 from .exceptions import MetadataFormatError
+
+
+
+def get_csv_delimiter(csv_path: str, delimiters: List[str] = [",", ";"]):
+    """
+    Determine which delimiter is being used in a CSV
+    
+    I couldn't find a solution just using `pd.read_csv`; using
+    a regex sep=[,;] does not work.
+    
+    """
+    
+    with open(csv_path, "r") as csv:
+        for header in csv:
+            break
+        
+        used = [
+            delimiter
+            for delimiter in delimiters
+            if header.find(delimiter) != -1
+        ]
+        
+        if not used:
+            return None
+        
+        if not len(used) == 1:
+            raise MetadataFormatError(f"Found multiple delimiters ({' '.join(used)}) in header: {header.strip()}.")
+    
+    return used[0]
+
+
+def check_barcode_format(barcode: str, try_to_fix: bool=True) -> str:
+    """
+    Check that the format of a barcode is as expected, and optionally
+    try and fix if it is not
+    
+    """
+    
+    # Some hard-coded settings
+    MAX_BARCODE = 96
+    EXPECTED = "barcode[0-9]{2}$"
+    EXAMPLE = "barcode01"
+    
+    if not isinstance(barcode, str):
+        barcode = str(barcode)
+    
+    if not re.match(EXPECTED, barcode):
+        
+        if not try_to_fix:
+            raise MetadataFormatError(f"Barcode '{barcode}' has bad format: must conform to '{EXAMPLE}'.")
+            
+        # Raise a warning
+        warnings.warn(f"Barcode '{barcode}' has bad format: must conform to '{EXAMPLE}'. Trying to fix...")
+        
+        nums = re.findall("[0-9]+", barcode)
+        
+        if not nums:
+            raise MetadataFormatError(f"Barcode '{barcode}' has bad format: must conform to '{EXAMPLE}'.")
+        
+        if len(nums) > 1:
+            raise MetadataFormatError(f"Multiple numbers found in barcode: {barcode}.")
+            
+        barcode_int = int(nums[0])
+        if barcode_int > 96:
+            raise MetadataFormatError(f"Barcode '{barcode}' exceeds maximum of {MAX_BARCODE}.")
+            
+        barcode = f"barcode{barcode_int:02d}"
+        
+    return barcode
 
 
 class MetadataTableParser:
@@ -18,10 +90,11 @@ class MetadataTableParser:
         """
 
         self.csv = metadata_csv
-        self.df = pd.read_csv(self.csv)
+        self.df = pd.read_csv(self.csv, delimiter=get_csv_delimiter(self.csv))
 
         self._check_for_columns()
         self._check_entries_unique()
+        self._check_all_barcodes()
 
         self.barcodes = self.df["barcode"].tolist()
         if include_unclassified:
@@ -54,3 +127,7 @@ class MetadataTableParser:
                         f"Column {c} must contain only unique entires, but {entry} is duplicated."
                     )
                 observed_entries.append(entry)
+
+    def _check_all_barcodes(self) -> List[str]:
+        self.df["barcode"] = [check_barcode_format(b) for b in self.df["barcode"]]
+
---
+++ tests/test_metadata.py
@@ -0,0 +1,74 @@
+import pytest
+from nomadic.util.metadata import check_barcode_format, MetadataTableParser
+from nomadic.util.exceptions import MetadataFormatError
+
+
+# --------------------------------------------------------------------------------
+# Tests for check_barcode_format
+#
+# --------------------------------------------------------------------------------
+
+
+@pytest.mark.parametrize("barcode,try_to_fix,expected", [
+    ("barcode01", True, "barcode01"),  # Valid barcode
+    ("barcode25", False, "barcode25"),  # Valid barcode, no fix needed
+])
+def test_check_barcode_format_good(barcode, try_to_fix, expected):
+    assert check_barcode_format(barcode, try_to_fix) == expected
+
+@pytest.mark.parametrize("barcode,try_to_fix,expected", [
+    (26, True, "barcode26"),  # Fixable
+    ("barcode_1", True, "barcode01"),  # Fixable
+])
+def test_check_barcode_warning(barcode, try_to_fix, expected):
+    with pytest.warns(UserWarning):
+        assert check_barcode_format(barcode, try_to_fix) == expected
+
+
+@pytest.mark.parametrize("barcode,try_to_fix,expected", [
+    (200, True, "barcode26"),
+    ("barcode", True, "barcode01"),
+    ("barcode01barcode02", True, "barcode01"),
+])
+def test_check_barcode_warning_error(barcode, try_to_fix, expected):
+    with pytest.warns(UserWarning):
+        with pytest.raises(MetadataFormatError):
+            assert check_barcode_format(barcode, try_to_fix) == expected
+
+# --------------------------------------------------------------------------------
+# Tests for MetadataTableParser
+#
+# --------------------------------------------------------------------------------
+
+
+@pytest.mark.parametrize("csv_path,csv_shape",[
+    ("tests/_data/metadata/sample_info_good.csv", (5, 5)),
+    ("tests/_data/metadata/sample_info_semicolon.csv", (5, 5)),
+    ("tests/_data/metadata/sample_info_eurosep.csv", (5, 6)),
+    ("tests/_data/metadata/sample_info_hybridsep-col.csv", (5, 6)),
+])
+def test_metadata_correct(csv_path, csv_shape):
+    metadata = MetadataTableParser(csv_path)
+    assert metadata.df.shape == csv_shape
+
+
+@pytest.mark.parametrize("csv_path,csv_shape",[
+    ("tests/_data/metadata/sample_info_badbarcode-format.csv", (5, 5)),
+    ("tests/_data/metadata/sample_info_badbarcode-int.csv", (5, 5)),
+])
+def test_metadata_warns(csv_path, csv_shape):
+    with pytest.warns(UserWarning):
+        metadata = MetadataTableParser(csv_path)
+    assert metadata.df.shape == csv_shape 
+
+@pytest.mark.parametrize("csv_path,error_msg",[
+    ("tests/_data/metadata/sample_info_onecolumn.csv", "Metadata must contain column called sample_id!"),
+    ("tests/_data/metadata/sample_info_dupbarcode.csv", "Column barcode must contain only unique entires, but barcode04 is duplicated."),
+    ("tests/_data/metadata/sample_info_nobarcode.csv", "Metadata must contain column called barcode!"),
+    ("tests/_data/metadata/sample_info_badheader.csv", "Found multiple delimiters (, ;) in header: ﻿barcode;sample_id,parasite_per_ul,location;platform;number.")
+])
+def test_metadata_errors(csv_path, error_msg):
+    with pytest.raises(MetadataFormatError) as excinfo:
+        metadata = MetadataTableParser(csv_path)
+    assert str(excinfo.value) == error_msg
+        
