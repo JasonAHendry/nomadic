bc8a2240592544aeceaeb0df35ace01a76db6e31
JasonAHendry <jason.ahendry@gmail.com>
2024-07-21 06:47:35 +0200

    Removed unused import.

--- src/nomadic/download/main.py
+++ src/nomadic/download/main.py
@@ -1,4 +1,3 @@
-from nomadic.util.logging_config import LoggingFascade
 from .downloader import ReferenceDownloader
 from .references import REFERENCE_COLLECTION
 from .corrections import DHPS, update_reference_genome
