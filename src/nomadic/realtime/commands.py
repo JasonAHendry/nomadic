1fd27825224183ed7b9f163c3e2a455894f9d2d8
Bernd Bohmeier <berndbohmeier@googlemail.com>
2025-07-07 17:44:02 +0200

    Use experiment name as default for arguments
    We can use the experiment name to help find metadata files.
    We can also make it simpler to find a bed file by just using the panel
    name and look in the beds folder

--- src/nomadic/realtime/commands.py
+++ src/nomadic/realtime/commands.py
@@ -1,22 +1,22 @@
+import os
+
 import click
 from nomadic.download.references import REFERENCE_COLLECTION
-from nomadic.util.workspace import check_if_workspace
+from nomadic.util.workspace import check_if_workspace, Workspace, looks_like_a_bed_filepath
 
 
 @click.command(short_help="Run analysis in real-time.")
-@click.option(
-    "-e",
-    "--expt_name",
+@click.argument(
+    "experiment_name",
     type=str,
-    required=True,
-    help="Name of the experiment, used as output directory name. E.g. '2023-05-12_exptA'.",
 )
 @click.option(
     "-w",
     "--workspace",
+    "workspace_path",
     default="./",
     type=click.Path(exists=True, file_okay=False, dir_okay=True),
-    help="Path of the the workspace where all the files will be stored. If not given, users default workspace will be used, set by init",
+    help="Path of the the workspace where all the files will be stored. Defaults to current directory.",
 )
 @click.option(
     "-f",
@@ -29,15 +29,14 @@ from nomadic.util.workspace import check_if_workspace
     "-m",
     "--metadata_csv",
     type=click.Path(exists=True, dir_okay=False, file_okay=True),
-    required=True,
-    help="Path to metadata CSV file containing barcode and sample information.",
+    help="Path to metadata CSV file containing barcode and sample information. Defaults to 'metadata/[EXPERIMENT_NAME].csv'.",
 )
 @click.option(
     "-b",
     "--region_bed",
-    type=click.Path(exists=True, dir_okay=False, file_okay=True),
+    type=click.Path(),
     required=True,
-    help="Path to BED file specifying genomic regions of interest.",
+    help="Path to BED file specifying genomic regions of interest or name of panel, e.g. 'nomads8' or 'nomadsMVP'.",
 )
 @click.option(
     "-r",
@@ -62,8 +61,8 @@ from nomadic.util.workspace import check_if_workspace
     help="Increase logging verbosity. Helpful for debugging.",
 )
 def realtime(
-    expt_name,
-    workspace,
+    experiment_name,
+    workspace_path,
     fastq_dir,
     metadata_csv,
     region_bed,
@@ -76,15 +75,37 @@ def realtime(
     """
     from .main import main
 
-    if not check_if_workspace(workspace):
+    if not check_if_workspace(workspace_path):
         raise click.BadParameter(
             param_hint="-w/--workspace",
-            message=f"Workspace '{workspace}' does not exist or is not a workspace. Please use nomadic start to create a new workspace, or navigate to your workspace",
+            message=f"Workspace '{workspace_path}' does not exist or is not a workspace. Please use nomadic start to create a new workspace, or navigate to your workspace",
         )
 
+    workspace = Workspace(workspace_path)
+
+    if not metadata_csv:
+        metadata_csv = workspace.get_meta_data_csv(experiment_name)
+        if not os.path.isfile(metadata_csv):
+            raise click.BadParameter(
+                message=f"Metadata CSV file not found at {metadata_csv}. Does it match the experment name?",
+            )
+
+    if not os.path.isfile(region_bed):
+        if not looks_like_a_bed_filepath(region_bed):
+            # Assume it's a panel name
+            region_bed = workspace.get_bed_file(region_bed)
+            if not os.path.isfile(region_bed):
+                raise click.BadParameter(
+                    message=f"Region BED file not found at {region_bed}. Possible panel names: {workspace.get_panel_names()}.",
+                )
+        else:
+            raise click.BadParameter(
+                message=f"Region BED file not found at {region_bed}.",
+            )
+
     main(
-        expt_name,
-        workspace,
+        experiment_name,
+        workspace_path,
         fastq_dir,
         metadata_csv,
         region_bed,
---
+++ src/nomadic/util/workspace.py
@@ -0,0 +1,85 @@
+import os
+
+from nomadic.util.dirs import produce_dir
+
+
+def init_workspace(path: str):
+    if os.path.exists(path):
+        raise RuntimeError(f"folder/file {path} already exists")
+
+    produce_dir(path, "results")
+    produce_dir(path, "beds")
+    produce_dir(path, "metadata")
+
+
+def check_if_workspace(path: str) -> bool:
+    """
+    Check if the given path is a valid Nomadic workspace.
+    A valid workspace contains a 'results', 'beds' and 'metadata' directory.
+    """
+    return (
+        os.path.exists(path)
+        and os.path.isdir(path)
+        and os.path.exists(os.path.join(path, "results"))
+        and os.path.exists(os.path.join(path, "beds"))
+        and os.path.exists(os.path.join(path, "metadata"))
+    )
+
+
+def looks_like_a_bed_filepath(path: str) -> bool:
+    """
+    Check if the given path looks like a file path to a bed file.
+    A file path typically contains a file extension or a directory structure.
+    """
+    return ".bed" in path or "/" in path
+
+
+class Workspace:
+    def __init__(self, path: str):
+        self.path = path
+
+    def __str__(self):
+        return f"Nomadic Workspace at {self.path}"
+
+    def __repr__(self):
+        return f"Workspace(path={self.path})"
+
+    def get_results_dir(self):
+        """
+        Get the results directory of the workspace.
+        """
+        return os.path.join(self.path, "results")
+
+    def get_beds_dir(self):
+        """
+        Get the beds directory of the workspace.
+        """
+        return os.path.join(self.path, "beds")
+
+    def get_metadata_dir(self):
+        """
+        Get the metadata directory of the workspace.
+        """
+        return os.path.join(self.path, "metadata")
+
+    def get_meta_data_csv(self, experiment_name: str):
+        """
+        Get the path to the metadata CSV file for a given experiment.
+        """
+        return os.path.join(self.get_metadata_dir(), f"{experiment_name}.csv")
+
+    def get_bed_file(self, panel_name: str):
+        """
+        Get the path to the BED file for a given panel name.
+        """
+        return os.path.join(self.get_beds_dir(), f"{panel_name}.bed")
+
+    def get_panel_names(self):
+        """
+        Get a list of available panel names in the workspace.
+        """
+        return [
+            file.removesuffix(".bed")
+            for file in os.listdir(self.get_beds_dir())
+            if file.endswith(".bed")
+        ]
