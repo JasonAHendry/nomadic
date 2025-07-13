e0d37fb659ea1b08a60867d0859d30ad41a24e40
JasonAHendry <jason.ahendry@gmail.com>
2025-07-13 23:54:19 +0200

    Fix incorrect default FASTQ dir
    The correct path includes two intermediate directories. This
    may not be an ideal solution.

--- src/nomadic/util/minknow.py
+++ src/nomadic/util/minknow.py
@@ -1,6 +1,21 @@
+import glob
+import warnings
+
+
 def default_fastq_dir(experiment_name: str) -> str:
     """
     Return the default FASTQ directory for MinKNOW experiments.
     This is typically where Minknow/Guppy writes the FASTQ files.
+
     """
-    return f"/var/lib/minknow/data/{experiment_name}/fastq_pass"
+    fastq_pass_dirs = glob.glob(
+        f"/var/lib/minknow/data/{experiment_name}/*/*/fastq_pass"
+    )
+    if len(fastq_pass_dirs) > 1:
+        warnings.warn(
+            f"Found {len(fastq_pass_dirs)} 'fastq_pass' directories,"
+            " This suggests more than one experiment with this name has"
+            " been run from MinKNOW. Will proceed with most recent."
+        )
+
+    return fastq_pass_dirs[-1]
