6d8154e67322c86052864f67c470e1566cc65adc
Bernd Bohmeier <berndbohmeier@googlemail.com>
2025-12-01 16:32:11 +0100

    Allow to configure backup command

--- src/nomadic/configure/commands.py
+++ src/nomadic/configure/commands.py
@@ -65,8 +65,60 @@ def share(workspace_path: str, target_dir: str):
                 param_hint="-t/--target_dir",
                 message=f"Directory {target_dir} could not be found",
             )
-        to_store = str(Path(target_dir).resolve())
+        to_store = str(target_path.resolve())
 
     set_config_value(config, ["share", "defaults", "target_dir"], to_store)
+
+    write_config(config, config_path)
+
+
+@configure.command(short_help="Configure the nomadic backup command.")
+@click.option(
+    "-w",
+    "--workspace",
+    "workspace_path",
+    default="./",
+    show_default="current directory",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+    help="Path of the workspace where all input/output files (beds, metadata, results) are stored. "
+    "The workspace directory simplifies the use of nomadic in that many arguments don't need to be listed "
+    "as they are predefined in the workspace config or can be loaded from the workspace",
+)
+@click.option(
+    "-t",
+    "--target_dir",
+    "target_dir",
+    type=click.Path(),  # Use path here to enable autocomplete, even though not only paths are allowed
+    help=(
+        "Path to target folder or an SSH target like user@host:/path. "
+        "The shared files will go inside of that folder into a folder with the name of the workspace."
+    ),
+    prompt="Set the target into which to share",
+)
+def backup(workspace_path: str, target_dir: str):
+    if not check_if_workspace(workspace_path):
+        raise click.BadParameter(
+            param_hint="-w/--workspace",
+            message=f"Workspace '{workspace_path}' does not exist or is not a workspace. Please use nomadic start to create a new workspace, or navigate to your workspace",
+        )
+
+    config_path = os.path.join(workspace_path, default_config_path)
+    if os.path.isfile(config_path):
+        config = load_config(config_path)
+    else:
+        config = {}
+
+    if is_ssh_target(target_dir):
+        to_store = target_dir
+    else:
+        target_path = Path(target_dir)
+        if not target_path.is_dir():
+            raise click.BadParameter(
+                param_hint="-t/--target_dir",
+                message=f"Directory {target_dir} could not be found",
+            )
+        to_store = str(target_path.resolve())
+
+    set_config_value(config, ["backup", "defaults", "target_dir"], to_store)
 
     write_config(config, config_path)
