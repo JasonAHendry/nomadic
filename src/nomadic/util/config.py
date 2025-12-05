07ce7bfd431aace374760527b280f5f2d776f37f
Daniel Bridges <danieljbridges@gmail.com>
2025-12-05 14:25:12 +0200

    Use of .config.yaml file for shared_folder. Includes function for searching nexted config dict

--- src/nomadic/util/config.py
+++ src/nomadic/util/config.py
@@ -1,9 +1,12 @@
-from yaml import load, dump
+from functools import reduce
+
+from yaml import dump, load
 
 try:
-    from yaml import CLoader as Loader, CDumper as Dumper
+    from yaml import CDumper as Dumper
+    from yaml import CLoader as Loader
 except ImportError:
-    from yaml import Loader, Dumper
+    from yaml import Dumper, Loader
 
 default_config_path = ".config.yaml"
 
@@ -32,3 +35,21 @@ def write_config(config: dict, config_path: str) -> None:
     """
     with open(config_path, "w") as f:
         dump(config, f, Dumper=Dumper, default_flow_style=False)
+
+
+def get_nested_config_value(d: dict, keys: list, default=None) -> str | None:
+    """
+    Identify a nested value in a dictionary given a list of keys.
+
+    Args:
+        d (dict): The dictionary to search.
+        keys (list): A list of keys representing the sequential path to the desired value.
+        default: The value to return if the path does not exist.
+
+    Returns:
+        The value found at the nested path, or the default value if the path does not exist
+    """
+    return (
+        reduce(lambda acc, k: acc.get(k, {}) if isinstance(acc, dict) else {}, keys, d)
+        or default
+    )
