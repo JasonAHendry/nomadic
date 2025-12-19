7b58bfdf5e8778a998ca684757df1d2eb597b833
Bernd Bohmeier <berndbohmeier@googlemail.com>
2025-12-19 18:14:52 +0100

    Only plot data in summary if we have it

--- src/nomadic/summarize/dashboard/components.py
+++ src/nomadic/summarize/dashboard/components.py
@@ -238,6 +238,7 @@ class AmpliconsBarplot(SummaryDashboardComponent):
                     marker=dict(color=SAMPLE_COLORS[status.value]),
                 )
                 for status in Status
+                if status.value in plot_df.columns
             ]
         )
 
@@ -621,34 +622,38 @@ class PrevalenceHeatmap(SummaryDashboardComponent):
             )
             plot_df = plot_df.reindex(pos_order)
 
-            # Hover statment
-            customdata = np.stack(
-                [plot_df["n_mixed"], plot_df["n_mut"], plot_df["n_passed"]], axis=-1
-            )
-            htemp = "<b>%{y} (%{x})</b><br>"
-            htemp += "<b>Prevalence:</b> %{z:.0f}%<br>"
-            htemp += "<b>Samples:</b> %{customdata[2]}<br>"
-            htemp += "<b>Mixed:</b> %{customdata[0]}<br>"
-            htemp += "<b>Clonal:</b> %{customdata[1]}<br>"
-
-            plot_data = [
-                go.Heatmap(
-                    x=plot_df["prevalence"].columns,
-                    y=plot_df["prevalence"].index,
-                    z=plot_df["prevalence"],
-                    texttemplate="%{z:.0f}%",
-                    customdata=customdata,
-                    zmin=0,
-                    zmax=100,
-                    xgap=1,
-                    ygap=1,
-                    colorscale="Spectral_r",
-                    colorbar=dict(title="", outlinecolor="black", outlinewidth=1),
-                    hoverongaps=False,
-                    hovertemplate=htemp,
-                    name="",
+            if len(plot_df) > 0:
+                # Hover statment
+                customdata = np.stack(
+                    [plot_df["n_mixed"], plot_df["n_mut"], plot_df["n_passed"]], axis=-1
                 )
-            ]
+                htemp = "<b>%{y} (%{x})</b><br>"
+                htemp += "<b>Prevalence:</b> %{z:.0f}%<br>"
+                htemp += "<b>Samples:</b> %{customdata[2]}<br>"
+                htemp += "<b>Mixed:</b> %{customdata[0]}<br>"
+                htemp += "<b>Clonal:</b> %{customdata[1]}<br>"
+
+                plot_data = [
+                    go.Heatmap(
+                        x=plot_df["prevalence"].columns,
+                        y=plot_df["prevalence"].index,
+                        z=plot_df["prevalence"],
+                        texttemplate="%{z:.0f}%",
+                        customdata=customdata,
+                        zmin=0,
+                        zmax=100,
+                        xgap=1,
+                        ygap=1,
+                        colorscale="Spectral_r",
+                        colorbar=dict(title="", outlinecolor="black", outlinewidth=1),
+                        hoverongaps=False,
+                        hovertemplate=htemp,
+                        name="",
+                    )
+                ]
+            else:
+                plot_data = []
+
             MAR = 40
             fig = go.Figure(plot_data)
             fig.update_layout(
